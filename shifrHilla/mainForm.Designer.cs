bfc2fa4504065df0ac3f1845a5a277a6a9d5e4ab
TechieDevice <45479102+TechieDevice@users.noreply.github.com>
2021-03-17 14:51:35 +0700

    Hill

---
+++ shifrHilla/mainForm.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace shifrHilla
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.enterConsole = new System.Windows.Forms.TextBox();
+            this.encryptionButton = new System.Windows.Forms.Button();
+            this.checkBox = new System.Windows.Forms.CheckBox();
+            this.decryptionButton = new System.Windows.Forms.Button();
+            this.encryptionConsole = new System.Windows.Forms.TextBox();
+            this.keyBox1 = new System.Windows.Forms.TextBox();
+            this.genKeyButton = new System.Windows.Forms.Button();
+            this.keyBox2 = new System.Windows.Forms.TextBox();
+            this.checkKey = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // enterConsole
+            // 
+            this.enterConsole.BackColor = System.Drawing.SystemColors.MenuText;
+            this.enterConsole.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.enterConsole.Location = new System.Drawing.Point(19, 130);
+            this.enterConsole.Multiline = true;
+            this.enterConsole.Name = "enterConsole";
+            this.enterConsole.Size = new System.Drawing.Size(235, 228);
+            this.enterConsole.TabIndex = 0;
+            // 
+            // encryptionButton
+            // 
+            this.encryptionButton.Location = new System.Drawing.Point(19, 38);
+            this.encryptionButton.Name = "encryptionButton";
+            this.encryptionButton.Size = new System.Drawing.Size(103, 23);
+            this.encryptionButton.TabIndex = 1;
+            this.encryptionButton.Text = "Зашифровать";
+            this.encryptionButton.UseVisualStyleBackColor = true;
+            this.encryptionButton.Click += new System.EventHandler(this.EncryptionButton_Click);
+            // 
+            // checkBox
+            // 
+            this.checkBox.AutoSize = true;
+            this.checkBox.Location = new System.Drawing.Point(19, 12);
+            this.checkBox.Name = "checkBox";
+            this.checkBox.Size = new System.Drawing.Size(93, 17);
+            this.checkBox.TabIndex = 2;
+            this.checkBox.Text = "Рекурентный";
+            this.checkBox.UseVisualStyleBackColor = true;
+            // 
+            // decryptionButton
+            // 
+            this.decryptionButton.Location = new System.Drawing.Point(19, 67);
+            this.decryptionButton.Name = "decryptionButton";
+            this.decryptionButton.Size = new System.Drawing.Size(103, 23);
+            this.decryptionButton.TabIndex = 3;
+            this.decryptionButton.Text = "Расшифровать";
+            this.decryptionButton.UseVisualStyleBackColor = true;
+            this.decryptionButton.Click += new System.EventHandler(this.DecryptionButton_Click);
+            // 
+            // encryptionConsole
+            // 
+            this.encryptionConsole.BackColor = System.Drawing.SystemColors.MenuText;
+            this.encryptionConsole.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.encryptionConsole.Location = new System.Drawing.Point(260, 130);
+            this.encryptionConsole.Multiline = true;
+            this.encryptionConsole.Name = "encryptionConsole";
+            this.encryptionConsole.Size = new System.Drawing.Size(236, 228);
+            this.encryptionConsole.TabIndex = 4;
+            // 
+            // keyBox1
+            // 
+            this.keyBox1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.keyBox1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.keyBox1.Location = new System.Drawing.Point(236, 21);
+            this.keyBox1.Multiline = true;
+            this.keyBox1.Name = "keyBox1";
+            this.keyBox1.Size = new System.Drawing.Size(126, 69);
+            this.keyBox1.TabIndex = 5;
+            this.keyBox1.Text = "key 1";
+            // 
+            // genKeyButton
+            // 
+            this.genKeyButton.Location = new System.Drawing.Point(128, 21);
+            this.genKeyButton.Name = "genKeyButton";
+            this.genKeyButton.Size = new System.Drawing.Size(102, 35);
+            this.genKeyButton.TabIndex = 6;
+            this.genKeyButton.Text = "Сгенерировать ключ";
+            this.genKeyButton.UseVisualStyleBackColor = true;
+            this.genKeyButton.Click += new System.EventHandler(this.GenKeyButton_Click);
+            // 
+            // keyBox2
+            // 
+            this.keyBox2.BackColor = System.Drawing.SystemColors.MenuText;
+            this.keyBox2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.keyBox2.Location = new System.Drawing.Point(368, 21);
+            this.keyBox2.Multiline = true;
+            this.keyBox2.Name = "keyBox2";
+            this.keyBox2.Size = new System.Drawing.Size(128, 69);
+            this.keyBox2.TabIndex = 7;
+            this.keyBox2.Text = "key 2";
+            // 
+            // checkKey
+            // 
+            this.checkKey.Location = new System.Drawing.Point(128, 62);
+            this.checkKey.Name = "checkKey";
+            this.checkKey.Size = new System.Drawing.Size(102, 28);
+            this.checkKey.TabIndex = 8;
+            this.checkKey.Text = "Ввести ключ";
+            this.checkKey.UseVisualStyleBackColor = true;
+            this.checkKey.Click += new System.EventHandler(this.CheckKey_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Исходный текст";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(295, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Текст после обработки";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 370);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkKey);
+            this.Controls.Add(this.keyBox2);
+            this.Controls.Add(this.genKeyButton);
+            this.Controls.Add(this.keyBox1);
+            this.Controls.Add(this.encryptionConsole);
+            this.Controls.Add(this.decryptionButton);
+            this.Controls.Add(this.checkBox);
+            this.Controls.Add(this.encryptionButton);
+            this.Controls.Add(this.enterConsole);
+            this.Name = "mainForm";
+            this.Text = "Шифр Хилла";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox enterConsole;
+        private System.Windows.Forms.Button encryptionButton;
+        private System.Windows.Forms.CheckBox checkBox;
+        private System.Windows.Forms.Button decryptionButton;
+        private System.Windows.Forms.TextBox encryptionConsole;
+        private System.Windows.Forms.TextBox keyBox1;
+        private System.Windows.Forms.Button genKeyButton;
+        private System.Windows.Forms.TextBox keyBox2;
+        private System.Windows.Forms.Button checkKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ shifrHilla/mainForm.cs
@@ -0,0 +1,566 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace shifrHilla
+{
+    public partial class mainForm : Form
+    {
+        private long[,] keyA;
+        private long[,] keyB;
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private static long getDet(long[,] A)
+        {
+            long det = (A[0, 0] * A[1, 1] * A[2, 2] + A[1, 0] * A[0, 2] * A[2, 1] + A[0, 1] * A[1, 2] * A[2, 0] - (A[0, 2] * A[1, 1] * A[2, 0] + A[0, 0] * A[1, 2] * A[2, 1] + A[0, 1] * A[1, 0] * A[2, 2])) % 65521;
+            if (det < 0) det = det + 65521;
+            return det;
+        }
+
+        private static long getDet0(long det)
+        {
+            long det0 = 1;
+            long b = 65521, a = det, c;
+            List<long> A = new List<long>();
+            List<long> B = new List<long>();
+            List<long> C = new List<long>();
+            A.Add(a);
+            B.Add(b);
+            C.Add(1);
+
+            while (a != 1)
+            {
+                while (a < b)
+                {
+                    b = b - a;
+                }
+
+                c = b;
+                b = a;
+                a = c;
+                A.Add(a);
+                B.Add(b);
+                if (C[C.Count - 1] == 1) { C.Add(-1); }
+                else { C.Add(1); }
+            }
+
+            while (A.Count != 0)
+            {
+                det0 = (B[B.Count - 1] * det0 + C[C.Count - 1]) / A[A.Count - 1];
+                A.RemoveAt(A.Count - 1);
+                B.RemoveAt(B.Count - 1);
+                C.RemoveAt(C.Count - 1);
+            }
+
+            return det0 % 65521;
+        }
+
+        private static long[,] getKey0(long[,] key)
+        {
+            long[,] key0 = new long[3, 3];
+            long[,] min = new long[3, 3];
+            long keyDet, keyDet0;
+
+            min[0, 0] = (key[1, 1] * key[2, 2] - key[1, 2] * key[2, 1]) % 65521;
+            if (min[0, 0] < 0) min[0, 0] = min[0, 0] + 65521;
+            min[0, 1] = (-1*(key[1, 0] * key[2, 2] - key[1, 2] * key[2, 0])) % 65521;
+            if (min[0, 1] < 0) min[0, 1] = min[0, 1] + 65521;
+            min[0, 2] = (- key[1, 1] * key[2, 0] + key[1, 0] * key[2, 1]) % 65521;
+            if (min[0, 2] < 0) min[0, 2] = min[0, 2] + 65521;
+            min[1, 0] = (-1*(key[0, 1] * key[2, 2] - key[0, 2] * key[2, 1])) % 65521;
+            if (min[1, 0] < 0) min[1, 0] = min[1, 0] + 65521;
+            min[1, 1] = (key[0, 0] * key[2, 2] - key[0, 2] * key[2, 0]) % 65521;
+            if (min[1, 1] < 0) min[1, 1] = min[1, 1] + 65521;
+            min[1, 2] = (-1*(- key[0, 1] * key[2, 0] + key[0, 0] * key[2, 1])) % 65521;
+            if (min[1, 2] < 0) min[1, 2] = min[1, 2] + 65521;
+            min[2, 0] = (- key[1, 1] * key[0, 2] + key[1, 2] * key[0, 1]) % 65521;
+            if (min[2, 0] < 0) min[2, 0] = min[2, 0] + 65521;
+            min[2, 1] = (-1*(- key[1, 0] * key[0, 2] + key[1, 2] * key[0, 0])) % 65521;
+            if (min[2, 1] < 0) min[2, 1] = min[2, 1] + 65521;
+            min[2, 2] = (key[1, 1] * key[0, 0] - key[1, 0] * key[0, 1]) % 65521;
+            if (min[2, 2] < 0) min[2, 2] = min[2, 2] + 65521;
+
+            for (int m = 0; m < 3; m++)
+            {
+                for (int n = 0; n < 3; n++)
+                {
+                    key0[m, n] = min[n, m];
+                }
+            }
+
+            keyDet = getDet(key);
+            keyDet0 = getDet0(keyDet);
+
+            for (int m = 0; m < 3; m++)
+            {
+                for (int n = 0; n < 3; n++)
+                {
+                    key0[m, n] = (key0[m, n] * keyDet0) % 65521;
+                }
+            }
+
+            return key0;
+        }
+
+        private static long[,] mulKeys(long[,] key1, long[,] key2)
+        {
+            long[,] key = new long[3,3];
+
+            for (int m = 0; m < 3; m++)
+            {
+                for (int n = 0; n < 3; n++)
+                {
+                    for (int i = 0; i < 3; i++)
+                    {
+                        key[m, n] = (key[m, n] + key2[m, i] * key1[i, n]) % 65521;
+                    }
+                }
+            }
+            return key;
+        }
+
+        private static long[,] mulKeys0(long[,] key1, long[,] key2)
+        {
+            long[,] key = new long[3, 3];
+
+            for (int m = 0; m < 3; m++)
+            {
+                for (int n = 0; n < 3; n++)
+                {
+                    for (int i = 0; i < 3; i++)
+                    {
+                        key[m, n] = (key[m, n] + key1[m, i] * key2[i, n]) % 65521;
+                    }
+                }
+            }
+            return key;
+        }
+
+        private void DecryptionButton_Click(object sender, EventArgs e)
+        {
+            if (checkBox.Checked == true)
+            {
+                rekurentniy_decryption();
+            }
+            else
+            {
+                ne_rekurentniy_decryption();
+            }
+        }
+
+        private void EncryptionButton_Click(object sender, EventArgs e)
+        {
+            if (checkBox.Checked == true)
+            {
+                rekurentniy_encryption();
+            }
+            else
+            {
+                ne_rekurentniy_encryption();
+            }
+        }
+
+        private void ne_rekurentniy_encryption()
+        {
+            long[,] key = keyA;
+            long[] text = new long[3];
+
+            string str = enterConsole.Text;
+            string encStr = "kostil";
+            long encInt;
+            char encChar;
+
+            for (int i = 0; i < str.Length; i = i + 3)
+            {
+                text[0] = Convert.ToInt32(str[i]);
+                if (i + 1 < str.Length)
+                {
+                    text[1] = Convert.ToInt32(str[i + 1]);
+                }
+                else
+                {
+                    text[1] = 0;
+                }
+                if (i + 2 < str.Length)
+                {
+                    text[2] = Convert.ToInt32(str[i + 2]);
+                }
+                else
+                {
+                    text[2] = 0;
+                }
+
+                for (int m = 0; m < 3; m++)
+                {
+                    encInt = 0;
+
+                    for (int n = 0; n < 3; n++)
+                    {
+                        encInt = encInt + key[m, n] * text[n];
+                    }
+
+                    encChar = Convert.ToChar(encInt % 65521);
+                    if (encStr == "kostil")
+                    {
+                        encStr = Convert.ToString(encChar);
+                    }
+                    else
+                    {
+                        encStr = encStr + Convert.ToString(encChar);
+                    }
+                }
+            }
+
+            encryptionConsole.Text = encStr;
+        }
+
+        private void ne_rekurentniy_decryption()
+        {
+            long[,] key0 = getKey0(keyA);
+            long[] text = new long[3];
+
+            string str = enterConsole.Text;
+            string encStr = "kostil";
+            long encInt;
+            char encChar;
+
+            for (int i = 0; i < str.Length; i = i + 3)
+            {
+                text[0] = Convert.ToInt32(str[i]);
+                if (i + 1 < str.Length)
+                {
+                    text[1] = Convert.ToInt32(str[i + 1]);
+                }
+                else
+                {
+                    text[1] = 0;
+                }
+                if (i + 2 < str.Length)
+                {
+                    text[2] = Convert.ToInt32(str[i + 2]);
+                }
+                else
+                {
+                    text[2] = 0;
+                }
+
+
+                for (int m = 0; m < 3; m++)
+                {
+                    encInt = 0;
+
+                    for (int n = 0; n < 3; n++)
+                    {
+                        encInt = encInt + key0[m, n] * text[n];
+                    }
+
+                    encChar = Convert.ToChar(encInt % 65521);
+                    if (encStr == "kostil")
+                    {
+                        encStr = Convert.ToString(encChar);
+                    }
+                    else
+                    {
+                        encStr = encStr + Convert.ToString(encChar);
+                    }
+                }
+            }
+
+            encryptionConsole.Text = encStr;
+        }
+
+        private void rekurentniy_encryption()
+        {
+            long[,] key1 = keyA;
+            long[,] key2 = keyB;
+            long[] text = new long[3];
+            long[,] key;
+
+            string str = enterConsole.Text;
+            string encStr = "kostil";
+            long encInt;
+            char encChar;
+
+            for (int i = 0; i < str.Length; i = i + 3)
+            {
+                text[0] = Convert.ToInt32(str[i]);
+                if (i + 1 < str.Length)
+                {
+                    text[1] = Convert.ToInt32(str[i + 1]);
+                }
+                else
+                {
+                    text[1] = 0;
+                }
+                if (i + 2 < str.Length)
+                {
+                    text[2] = Convert.ToInt32(str[i + 2]);
+                }
+                else
+                {
+                    text[2] = 0;
+                }
+
+                if (i == 0)
+                {
+                    key = key1;
+
+                }
+                else if (i == 3)
+                {
+                    key = key2;
+                }
+                else
+                {
+                    key = mulKeys(key1, key2);
+                    key1 = key2;
+                    key2 = key;
+                }
+
+                for (int m = 0; m < 3; m++)
+                {
+                    encInt = 0;
+
+                    for (int n = 0; n < 3; n++)
+                    {
+                        encInt = encInt + key[m, n] * text[n];
+                    }
+
+                    encChar = Convert.ToChar(encInt % 65521);
+                    if (encStr == "kostil")
+                    {
+                        encStr = Convert.ToString(encChar);
+                    }
+                    else
+                    {
+                        encStr = encStr + Convert.ToString(encChar);
+                    }
+                }
+            }
+
+            encryptionConsole.Text = encStr;
+        }
+
+        private void rekurentniy_decryption()
+        {
+            long[,] key1 = getKey0(keyA);
+            long[,] key2 = getKey0(keyB);
+            long[] text = new long[3];
+            long[,] key0;
+
+            string str = enterConsole.Text;
+            string encStr = "kostil";
+            long encInt;
+            char encChar;
+
+            for (int i = 0; i < str.Length; i = i + 3)
+            {
+                text[0] = Convert.ToInt32(str[i]);
+                if (i + 1 < str.Length)
+                {
+                    text[1] = Convert.ToInt32(str[i + 1]);
+                }
+                else
+                {
+                    text[1] = 0;
+                }
+                if (i + 2 < str.Length)
+                {
+                    text[2] = Convert.ToInt32(str[i + 2]);
+                }
+                else
+                {
+                    text[2] = 0;
+                }
+
+                if (i == 0)
+                {
+                    key0 = key1;
+
+                }
+                else if (i == 3)
+                {
+                    key0 = key2;
+                }
+                else
+                {
+                    key0 = mulKeys0(key1, key2);
+                    key1 = key2;
+                    key2 = key0;
+                }
+
+                for (int m = 0; m < 3; m++)
+                {
+                    encInt = 0;
+
+                    for (int n = 0; n < 3; n++)
+                    {
+                        encInt = encInt + key0[m, n] * text[n];
+                    }
+
+                    encChar = Convert.ToChar(encInt % 65521);
+                    if (encStr == "kostil")
+                    {
+                        encStr = Convert.ToString(encChar);
+                    }
+                    else
+                    {
+                        encStr = encStr + Convert.ToString(encChar);
+                    }
+                }
+            }
+
+            encryptionConsole.Text = encStr;
+        }
+
+        private void GenKeyButton_Click(object sender, EventArgs e)
+        {
+            Random Ran = new Random();
+            string str = "kostil";
+            long[,] key = new long[3, 3] { { Ran.Next(65521), Ran.Next(65521), Ran.Next(65521) }, { Ran.Next(65521), Ran.Next(65521), Ran.Next(65521) }, { Ran.Next(65521), Ran.Next(65521), Ran.Next(65521) } };
+            while (getDet(key) == 0)
+            {
+                key = new long[3, 3] { { Ran.Next(65521), Ran.Next(65521), Ran.Next(65521) }, { Ran.Next(65521), Ran.Next(65521), Ran.Next(65521) }, { Ran.Next(65521), Ran.Next(65521), Ran.Next(65521) } };
+            }
+            for (int m = 0; m < 3; m++)
+            {
+                for (int n = 0; n < 3; n++)
+                {
+                    if (str == "kostil")
+                    {
+                        str = key[m, n] + "   ";
+                    }
+                    else
+                    {
+                        str = str + key[m, n] + "   ";
+                    }                   
+                }
+                str = str + "\r\n";
+            }
+
+            keyBox1.Text = str;
+            keyA = key;
+
+            str = "kostil";
+
+            if (checkBox.Checked == true)
+            {
+                key = new long[3, 3] { { Ran.Next(65521), Ran.Next(65521), Ran.Next(65521) }, { Ran.Next(65521), Ran.Next(65521), Ran.Next(65521) }, { Ran.Next(65521), Ran.Next(65521), Ran.Next(65521) } };
+                while (getDet(key) == 0)
+                {
+                    key = new long[3, 3] { { Ran.Next(65521), Ran.Next(65521), Ran.Next(65521) }, { Ran.Next(65521), Ran.Next(65521), Ran.Next(65521) }, { Ran.Next(65521), Ran.Next(65521), Ran.Next(65521) } };
+                }
+                for (int m = 0; m < 3; m++)
+                {
+                    for (int n = 0; n < 3; n++)
+                    {
+                        if (str == "kostil")
+                        {
+                            str = key[m, n] + "   ";
+                        }
+                        else
+                        {
+                            str = str + key[m, n] + "   ";
+                        }
+                    }
+                    str = str + "\r\n";
+                }
+
+                keyBox2.Text = str;
+                keyB = key;
+            }
+        }
+
+        private void CheckKey_Click(object sender, EventArgs e)
+        {
+            string strKey = keyBox1.Text;
+            string strKeyNumber = "" + strKey[0];
+            long[,] key = new long[3, 3];
+            long m = 0, n = 0;
+
+            for (int i = 0; i < strKey.Length;)
+            {
+                while (strKeyNumber[strKeyNumber.Length - 1] != ' ')
+                {
+                    i++;
+                    strKeyNumber = strKeyNumber + strKey[i];
+                }
+
+                key[m, n] = Convert.ToInt32(strKeyNumber);
+                n = n + 1;
+                if (n == 3)
+                {
+                    m = m + 1;
+                    n = 0;
+                }
+                i = i + 3;
+                strKeyNumber = "" + strKey[i];
+                if (strKey[i + 1] == '\r')
+                {
+                    i = i + 2;
+                }
+                if (m == 3) break;
+            }
+
+            if (getDet(key) == 0)
+            {
+                keyBox1.Text = "wrong key";
+            }
+            else
+            {
+                keyA = key;
+            }
+
+
+            if (checkBox.Checked == true)
+            {
+                strKey = keyBox2.Text;
+                strKeyNumber = "" + strKey[0];
+                key = new long[3, 3];
+                m = 0; n = 0;
+                for (int i = 0; i < strKey.Length;)
+                {
+                    while (strKeyNumber[strKeyNumber.Length - 1] != ' ')
+                    {
+                        i++;
+                        strKeyNumber = strKeyNumber + strKey[i];
+                    }
+
+                    key[m, n] = Convert.ToInt32(strKeyNumber);
+                    n = n + 1;
+                    if (n == 3)
+                    {
+                        m = m + 1;
+                        n = 0;
+                    }
+                    i = i + 3;
+                    strKeyNumber = "" + strKey[i];
+                    if (strKey[i + 1] == '\r')
+                    {
+                        i = i + 2;
+                    }
+                    if (m == 3) break;
+                }
+
+                if (getDet(key) == 0)
+                {
+                    keyBox2.Text = "wrong key";
+                }
+                else
+                {
+                    keyB = key;
+                }
+            }
+        }
+    }
+}
