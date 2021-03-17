7321aad151fd9b3715cbd0bb8b82a7f721a40f17
TechieDevice <45479102+TechieDevice@users.noreply.github.com>
2021-03-17 14:52:09 +0700

    RSA

---
+++ RSA/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace RSA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.encriptionButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.decryptionButton = new System.Windows.Forms.Button();
+            this.eBox = new System.Windows.Forms.TextBox();
+            this.dBox = new System.Windows.Forms.TextBox();
+            this.nOpenBox = new System.Windows.Forms.TextBox();
+            this.nCloseBox = new System.Windows.Forms.TextBox();
+            this.getKeysButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.decryptBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.encryptBox = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // encriptionButton
+            // 
+            this.encriptionButton.Location = new System.Drawing.Point(24, 21);
+            this.encriptionButton.Name = "encriptionButton";
+            this.encriptionButton.Size = new System.Drawing.Size(158, 23);
+            this.encriptionButton.TabIndex = 0;
+            this.encriptionButton.Text = "Зашифровать";
+            this.encriptionButton.UseVisualStyleBackColor = true;
+            this.encriptionButton.Click += new System.EventHandler(this.EncriptionButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(390, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Текст после выполнения операции";
+            // 
+            // decryptionButton
+            // 
+            this.decryptionButton.Location = new System.Drawing.Point(24, 60);
+            this.decryptionButton.Name = "decryptionButton";
+            this.decryptionButton.Size = new System.Drawing.Size(158, 23);
+            this.decryptionButton.TabIndex = 6;
+            this.decryptionButton.Text = "Расшифровать";
+            this.decryptionButton.UseVisualStyleBackColor = true;
+            this.decryptionButton.Click += new System.EventHandler(this.DecryptionButton_Click);
+            // 
+            // eBox
+            // 
+            this.eBox.Location = new System.Drawing.Point(165, 25);
+            this.eBox.Name = "eBox";
+            this.eBox.Size = new System.Drawing.Size(190, 20);
+            this.eBox.TabIndex = 7;
+            // 
+            // dBox
+            // 
+            this.dBox.Location = new System.Drawing.Point(165, 64);
+            this.dBox.Name = "dBox";
+            this.dBox.Size = new System.Drawing.Size(190, 20);
+            this.dBox.TabIndex = 8;
+            // 
+            // nOpenBox
+            // 
+            this.nOpenBox.Location = new System.Drawing.Point(361, 27);
+            this.nOpenBox.Name = "nOpenBox";
+            this.nOpenBox.Size = new System.Drawing.Size(175, 20);
+            this.nOpenBox.TabIndex = 9;
+            // 
+            // nCloseBox
+            // 
+            this.nCloseBox.Location = new System.Drawing.Point(361, 64);
+            this.nCloseBox.Name = "nCloseBox";
+            this.nCloseBox.Size = new System.Drawing.Size(175, 20);
+            this.nCloseBox.TabIndex = 10;
+            // 
+            // getKeysButton
+            // 
+            this.getKeysButton.Location = new System.Drawing.Point(33, 27);
+            this.getKeysButton.Name = "getKeysButton";
+            this.getKeysButton.Size = new System.Drawing.Size(123, 44);
+            this.getKeysButton.TabIndex = 12;
+            this.getKeysButton.Text = "Сгенерировать ключи";
+            this.getKeysButton.UseVisualStyleBackColor = true;
+            this.getKeysButton.Click += new System.EventHandler(this.GetKeysButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(162, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Открытый ключ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(162, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Закрытый ключ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.getKeysButton);
+            this.groupBox1.Controls.Add(this.nOpenBox);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.eBox);
+            this.groupBox1.Controls.Add(this.dBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.nCloseBox);
+            this.groupBox1.Location = new System.Drawing.Point(188, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(542, 95);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Генератор ключей";
+            // 
+            // decryptBox
+            // 
+            this.decryptBox.Location = new System.Drawing.Point(387, 131);
+            this.decryptBox.Multiline = true;
+            this.decryptBox.Name = "decryptBox";
+            this.decryptBox.Size = new System.Drawing.Size(343, 269);
+            this.decryptBox.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(21, 115);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Исходный текст";
+            // 
+            // encryptBox
+            // 
+            this.encryptBox.Location = new System.Drawing.Point(12, 131);
+            this.encryptBox.Multiline = true;
+            this.encryptBox.Name = "encryptBox";
+            this.encryptBox.Size = new System.Drawing.Size(343, 269);
+            this.encryptBox.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 412);
+            this.Controls.Add(this.encryptBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.decryptBox);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.decryptionButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.encriptionButton);
+            this.Name = "Form1";
+            this.Text = "RSA";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button encriptionButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button decryptionButton;
+        private System.Windows.Forms.TextBox eBox;
+        private System.Windows.Forms.TextBox dBox;
+        private System.Windows.Forms.TextBox nOpenBox;
+        private System.Windows.Forms.TextBox nCloseBox;
+        private System.Windows.Forms.Button getKeysButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox decryptBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox encryptBox;
+    }
+}
+
---
+++ RSA/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace RSA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static bool isSimple(BigInteger A)
+        {
+
+            for (int i = 2; i < A/2; i++)
+            {
+                if (A % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static bool EvklidAlgoritm(BigInteger A, BigInteger B)
+        {
+            while ((A != 0) && (B != 0))
+            {
+                if (A > B) A = A % B;
+                else B = B % A;
+            }
+            return ((A + B) == 1);
+        }
+
+        private static BigInteger[] GenerateKeys()
+        {
+            Random rnd = new Random();
+            BigInteger p, q;
+            BigInteger[] n = new BigInteger[3];
+            Byte[] bt = new Byte[4];
+
+            rnd.NextBytes(bt);
+            p = BitConverter.ToUInt32(bt, 0);
+            while (isSimple(p) == false)
+            {
+                rnd.NextBytes(bt);
+                p = BitConverter.ToUInt32(bt, 0);
+            }
+
+            rnd.NextBytes(bt);
+            q = BitConverter.ToUInt32(bt, 0);
+            while (isSimple(q) == false)
+            {
+                rnd.NextBytes(bt);
+                q = BitConverter.ToUInt32(bt, 0);
+            }
+
+            n[0] = p * q;
+            n[1] = (p - 1) * (q - 1);
+
+            n[2] = 3;
+            while (EvklidAlgoritm(n[2], n[1]) == false)
+            {
+                n[2] = n[2] + 2;
+            }
+            
+
+            return n;
+        }
+
+        private static BigInteger getD(BigInteger E, BigInteger fn)
+        {
+            BigInteger D = 1;
+            BigInteger b = fn, a = E, c;
+            List<BigInteger> A = new List<BigInteger>();
+            List<BigInteger> B = new List<BigInteger>();
+            List<BigInteger> C = new List<BigInteger>();
+            A.Add(a);
+            B.Add(b);
+            C.Add(1);
+
+            while (a != 1)
+            {
+                while (a < b)
+                {
+                    b = b - a;
+                }
+
+                c = b;
+                b = a;
+                a = c;
+                A.Add(a);
+                B.Add(b);
+                if (C[C.Count - 1] == 1) { C.Add(-1); }
+                else { C.Add(1); }
+            }
+
+            while (A.Count != 0)
+            {
+                D = (B[B.Count - 1] * D + C[C.Count - 1]) / A[A.Count - 1];
+                A.RemoveAt(A.Count - 1);
+                B.RemoveAt(B.Count - 1);
+                C.RemoveAt(C.Count - 1);
+            }
+
+            return D % fn;
+        }
+
+        private void GetKeysButton_Click(object sender, EventArgs e)
+        {
+            BigInteger[] n = new BigInteger[3]; 
+            n = GenerateKeys();
+            
+
+            BigInteger E = n[2];
+            BigInteger D = getD(E, n[1]);
+
+            nOpenBox.Text = Convert.ToString(n[0]);
+            nCloseBox.Text = Convert.ToString(n[0]);
+            eBox.Text = Convert.ToString(E);
+            dBox.Text = Convert.ToString(D);
+        }
+
+        private void DecryptionButton_Click(object sender, EventArgs e)
+        {
+            string str = encryptBox.Text;
+            string encStr = "kostil";
+            BigInteger origInt;
+            BigInteger encInt;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+
+                origInt = Convert.ToUInt64(str[i]);
+                encInt = BigInteger.ModPow(origInt, Convert.ToUInt64(eBox.Text), Convert.ToUInt64(nOpenBox.Text));
+                if (encStr == "kostil")
+                {
+                    encStr = Convert.ToString(encInt) + ' ';
+                }
+                else
+                {
+                    encStr = encStr + Convert.ToString(encInt) + ' ';
+                }
+            }
+
+
+            decryptBox.Text = encStr;
+        }
+
+        private void EncriptionButton_Click(object sender, EventArgs e)
+        {
+            string str = encryptBox.Text;
+            string[] strMas;
+            string[] emp = new string[] { " " };
+            string encStr = "kostil";
+            BigInteger origInt;
+            BigInteger encInt;
+
+            strMas = str.Split(emp, System.StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < str.Length; i++)
+            {
+
+                origInt = Convert.ToUInt64(strMas[i]);
+                encInt = BigInteger.ModPow(origInt, Convert.ToUInt64(dBox.Text), Convert.ToUInt64(nCloseBox.Text));
+
+                if (encStr == "kostil")
+                {
+                    encStr = Convert.ToString(encInt);
+                }
+                else
+                {
+                    encStr = encStr + Convert.ToString(encInt);
+                }
+            }
+
+
+            decryptBox.Text = encStr;
+        }
+    }
+}
